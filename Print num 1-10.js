7e37d673acadf14866796342a696702617a40514
Alen Dolby <alen224725@sahrdaya.ac.in>
2025-10-28 00:10:22 +0530

    Improve comment in Print num 1-10.js
    Updated comment for clarity.

--- Print num 1-10.js
+++ Print num 1-10.js
@@ -1,9 +1,8 @@
-// 1. Write a loop to print numbers from 1 to 10.
+//  Write a loop to print numbers from 1 to 10.
 function printOneToTen() {
   for (let i = 1; i <= 10; i++) {
     console.log(i);
   }
 }
-
 // Run the function
 printOneToTen();
