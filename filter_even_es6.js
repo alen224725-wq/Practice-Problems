4c6ddd501a25357a5b35060f892115762eb7823d
Alen Dolby <alen224725@sahrdaya.ac.in>
2025-10-28 00:12:50 +0530

    Update filter_even_es6.js

--- filter_even_es6.js
+++ filter_even_es6.js
@@ -1,4 +1,4 @@
-//Use ES6 filter to return even numbers from an array
+// Use ES6 filter to return even numbers from an array
 const numbers = [1, 2, 3, 4, 5, 6];
 const evenNumbers = numbers.filter(n => Number.isInteger(n) && n % 2 === 0);
 console.log(evenNumbers); 
