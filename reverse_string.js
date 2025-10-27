1bd5f0e04cc4b5e1830f06cf20df4d19d3c02c32
Alen Dolby <alen224725@sahrdaya.ac.in>
2025-10-28 00:14:04 +0530

    Update reverse_string.js

--- reverse_string.js
+++ reverse_string.js
@@ -1,8 +1,6 @@
-// 5. Write a function that reverses a given string.
+// Write a function that reverses a given string.
 function reverseString(str) {
   if (typeof str !== "string") throw new TypeError("Expected a string");
   return str.split("").reverse().join("");
 }
 
-// Example:
-// console.log(reverseString("hello")); // "olleh"
