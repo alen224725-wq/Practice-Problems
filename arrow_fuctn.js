1720bdd043647d0fa6aafaedd225a8d7099fa7f1
Alen Dolby <alen224725@sahrdaya.ac.in>
2025-10-28 00:10:49 +0530

    Refactor greet function to use arrow function syntax
    Updated comments to clarify the purpose of the arrow function.

--- arrow_fuctn.js
+++ arrow_fuctn.js
@@ -1,11 +1,7 @@
-// 9. Rewrite this function using an arrow function
+// Rewrite this function using an arrow function
 const greet = (name) => {
   return `Hello, ${name}`;
 };
-
-// Shorter version:
 const greetShort = name => `Hello, ${name}`;
 
-// Examples:
-// console.log(greet("Bob")); // Hello, Bob
-// console.log(greetShort("Eve")); // Hello, Eve
+
